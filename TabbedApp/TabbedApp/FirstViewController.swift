cdcd4e1cd03671fe208e1672729a940d060811dc
forkbattery <forkbattery@gmail.com>
2014-12-07 19:22:52 -0500

    Created a tabbed application and added a new View Controller to it

---
+++ TabbedApp/TabbedApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TabbedApp
+//
+//  Created by Kanishka Goel on 2014-12-07.
+//  Copyright (c) 2014 Kanishka Goel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
